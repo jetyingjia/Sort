5f2a0bb139229f70989046b433a8c3a1ddf6860f
jetyingjia <jet_yingjia@126.com>
2013-12-29 11:52:10 +0800

    sort

--- SORT/SORT/main.cpp
+++ SORT/SORT/main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-//#include <algorithm>
 #include <string>
 #include <vector>
 #include <iterator>
 #include "SORT.h"
-//#include "SORT.cpp"
+
 
 
 void main()
 {
-	extern int jet;
-	int jjj;
-	
-	
+
 	//***********int  sort******************//
 	SORT< std::vector<int> > iSort;
 	std::cout<<"Please initial N numbers: ";
@@ -28,56 +24,56 @@ void main()
 		std::cout<<ivec[i]<<" ";
 	}
 
-// 	//Ã°ÅÝÅÅÐò
-// 	std::vector<int> Bivec(ivec.begin(),ivec.end());
-// 	iSort.Bubble_sort(Bivec,N);
-// 	std::cout<<std::endl<<"The Bubble_sort is: ";
-// 	for (int i=0;i<N;i++)
-// 	{
-// 		std::cout<<Bivec[i]<<" ";
-// 	}
-// 
-// 	//¿ìËÙÅÅÐò
-// 	std::vector<int> Qivec(ivec.begin(),ivec.end());
-// 	iSort.Quick_sort(Qivec,0,N-1);
-// 	std::cout<<std::endl<<"The Quick_sort is: ";
-// 	for (int i=0;i<N;i++)
-// 	{
-// 		std::cout<<Qivec[i]<<" ";
-// 	}
-// 
-// 	//Ö±½Ó²åÈëÅÅÐò
-// 	std::vector<int> Zivec(ivec.begin(),ivec.end());
-// 	iSort.insertSort(Zivec,N);
-// 	std::cout<<std::endl<<"The insertSort is: ";
-// 	for (int i=0;i<N;i++)
-// 	{
-// 		std::cout<<Zivec[i]<<" ";
-// 	}
-// 	//Ï£¶ûÅÅÐò
-// 	std::vector<int> xiivec(ivec.begin(),ivec.end());
-// 	iSort.xierSort(xiivec,N);
-// 	std::cout<<std::endl<<"xier sort: ";
-// 	for (int i=0;i<N;i++)
-// 	{
-// 		std::cout<<xiivec[i];
-// 	}
-// 	//Ö±½ÓÑ¡ÔñÅÅÐò
-// 	std::vector<int> zxivec(ivec.begin(),ivec.end());
-// 	iSort.zxSort(zxivec,N);
-// 	std::cout<<std::endl<<"zhijie xuanze sort: ";
-// 	for (int i=0;i<N;i++)
-// 	{
-// 		std::cout<<zxivec[i]<<" ";
-// 	}
+	//Ã°ÅÝÅÅÐò
+	std::vector<int> Bivec(ivec.begin(),ivec.end());
+	iSort.Bubble_sort(Bivec,N);
+	std::cout<<std::endl<<"The Bubble_sort is: ";
+	for (int i=0;i<N;i++)
+	{
+		std::cout<<Bivec[i]<<" ";
+	}
+
+	//¿ìËÙÅÅÐò
+	std::vector<int> Qivec(ivec.begin(),ivec.end());
+	iSort.Quick_sort(Qivec,0,N-1);
+	std::cout<<std::endl<<"The Quick_sort is: ";
+	for (int i=0;i<N;i++)
+	{
+		std::cout<<Qivec[i]<<" ";
+	}
+
+	//Ö±½Ó²åÈëÅÅÐò
+	std::vector<int> Zivec(ivec.begin(),ivec.end());
+	iSort.insertSort(Zivec,N);
+	std::cout<<std::endl<<"The insertSort is: ";
+	for (int i=0;i<N;i++)
+	{
+		std::cout<<Zivec[i]<<" ";
+	}
+	//Ï£¶ûÅÅÐò
+	std::vector<int> xiivec(ivec.begin(),ivec.end());
+	iSort.xierSort(xiivec,N);
+	std::cout<<std::endl<<"xier sort: ";
+	for (int i=0;i<N;i++)
+	{
+		std::cout<<xiivec[i];
+	}
+	//Ö±½ÓÑ¡ÔñÅÅÐò
+	std::vector<int> zxivec(ivec.begin(),ivec.end());
+	iSort.zxSort(zxivec,N);
+	std::cout<<std::endl<<"zhijie xuanze sort: ";
+	for (int i=0;i<N;i++)
+	{
+		std::cout<<zxivec[i]<<" ";
+	}
 	//¹é²¢ÅÅÐò
-// 	std::vector<int> mivec(ivec.begin(),ivec.end());
-// 	iSort.mergeSort(mivec,N);
-// 	std::cout<<std::endl<<"mergeSort: ";
-// 	for (int i=0;i<N;i++)
-// 	{
-// 		std::cout<<mivec[i]<<" ";
-// 	}
+	std::vector<int> mivec(ivec.begin(),ivec.end());
+	iSort.mergeSort(mivec,N);
+	std::cout<<std::endl<<"mergeSort: ";
+	for (int i=0;i<N;i++)
+	{
+		std::cout<<mivec[i]<<" ";
+	}
 
 	//½¨Á¢×îÐ¡¶Ñ
 	std::vector <int> Hivec(ivec.begin(),ivec.end()); 
@@ -113,56 +109,56 @@ void main()
 		std::cout<<svec[i]<<" ";
 	}
 
-// 	//Ã°ÅÝÅÅÐò
-// 	std::vector<std::string> sBivec(svec.begin(),svec.end());
-// 	sSort.Bubble_sort(sBivec,M);
-// 	std::cout<<std::endl<<"The Bubble_sort is: ";
-// 	for (int i=0;i<M;i++)
-// 	{
-// 		std::cout<<sBivec[i]<<" ";
-// 	}
-// 
-// 	//¿ìËÙÅÅÐò
-// 	std::vector<std::string> sQivec(svec.begin(),svec.end());
-// 	sSort.Quick_sort(sQivec,0,M-1);
-// 	std::cout<<std::endl<<"The Quick_sort is: ";
-// 	for (int i=0;i<M;i++)
-// 	{
-// 		std::cout<<sQivec[i]<<" ";
-// 	}
-// 
-// 	//Ö±½Ó²åÈëÅÅÐò
-// 	std::vector<std::string> Zsvec(svec.begin(),svec.end());
-// 	sSort.insertSort(Zsvec,M);
-// 	std::cout<<std::endl<<"The insertSort is: ";
-// 	for (int i=0;i<M;i++)
-// 	{
-// 		std::cout<<Zsvec[i]<<" ";
-// 	}
-// 	//Ï£¶ûÅÅÐò
-// 	std::vector<std::string> xisvec(svec.begin(),svec.end());
-// 	sSort.xierSort(xisvec,M);
-// 	std::cout<<std::endl<<"xier sort: ";
-// 	for (int i=0;i<M;i++)
-// 	{
-// 		std::cout<<xisvec[i]<<" ";
-// 	}
-// 	//Ö±½ÓÑ¡ÔñÅÅÐò
-// 	std::vector<std::string> zxsvec(svec.begin(),svec.end());
-// 	sSort.zxSort(zxsvec,M);
-// 	std::cout<<std::endl<<"zhijie xuanze sort: ";
-// 	for (int i=0;i<M;i++)
-// 	{
-// 		std::cout<<zxsvec[i]<<" ";
-// 	}
+	//Ã°ÅÝÅÅÐò
+	std::vector<std::string> sBivec(svec.begin(),svec.end());
+	sSort.Bubble_sort(sBivec,M);
+	std::cout<<std::endl<<"The Bubble_sort is: ";
+	for (int i=0;i<M;i++)
+	{
+		std::cout<<sBivec[i]<<" ";
+	}
+
+	//¿ìËÙÅÅÐò
+	std::vector<std::string> sQivec(svec.begin(),svec.end());
+	sSort.Quick_sort(sQivec,0,M-1);
+	std::cout<<std::endl<<"The Quick_sort is: ";
+	for (int i=0;i<M;i++)
+	{
+		std::cout<<sQivec[i]<<" ";
+	}
+
+	//Ö±½Ó²åÈëÅÅÐò
+	std::vector<std::string> Zsvec(svec.begin(),svec.end());
+	sSort.insertSort(Zsvec,M);
+	std::cout<<std::endl<<"The insertSort is: ";
+	for (int i=0;i<M;i++)
+	{
+		std::cout<<Zsvec[i]<<" ";
+	}
+	//Ï£¶ûÅÅÐò
+	std::vector<std::string> xisvec(svec.begin(),svec.end());
+	sSort.xierSort(xisvec,M);
+	std::cout<<std::endl<<"xier sort: ";
+	for (int i=0;i<M;i++)
+	{
+		std::cout<<xisvec[i]<<" ";
+	}
+	//Ö±½ÓÑ¡ÔñÅÅÐò
+	std::vector<std::string> zxsvec(svec.begin(),svec.end());
+	sSort.zxSort(zxsvec,M);
+	std::cout<<std::endl<<"zhijie xuanze sort: ";
+	for (int i=0;i<M;i++)
+	{
+		std::cout<<zxsvec[i]<<" ";
+	}
 	//¹é²¢ÅÅÐò
-// 	std::vector<std::string> msvec(svec.begin(),svec.end());
-// 	sSort.mergeSort(msvec,N);
-// 	std::cout<<std::endl<<"mergeSort: ";
-// 	for (int i=0;i<N;i++)
-// 	{
-// 		std::cout<<msvec[i]<<" ";
-// 	}
+	std::vector<std::string> msvec(svec.begin(),svec.end());
+	sSort.mergeSort(msvec,N);
+	std::cout<<std::endl<<"mergeSort: ";
+	for (int i=0;i<N;i++)
+	{
+		std::cout<<msvec[i]<<" ";
+	}
 
 	system("pause");
 
